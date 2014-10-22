2e1550ce2a0f11c116e7f5cf9a52b7f9de39186e
Alex Boyd <aboyd@instructure.com>
2014-10-22 15:54:53 -0600

    Fix logging to stdout under Rails 4
    Change-Id: I45824bbc1a12ff3aa2a6306980a1c30440916353

--- lib/delayed/pool.rb
+++ lib/delayed/pool.rb
@@ -215,10 +215,16 @@ class Pool
 
   def tail_rails_log
     return if !@options[:tail_logs]
-    return if !Rails.logger.respond_to?(:log_path)
+    if Rails.logger.respond_to?(:log_path)
+      log_path = Rails.logger.log_path
+    elsif Rails.logger.instance_variable_get('@logdev').try(:instance_variable_get, '@dev').try(:path)
+      log_path = Rails.logger.instance_variable_get('@logdev').instance_variable_get('@dev').path
+    else
+      return
+    end
     Rails.logger.auto_flushing = true if Rails.logger.respond_to?(:auto_flushing=)
     Thread.new do
-      f = File.open(Rails.logger.log_path, 'r')
+      f = File.open(log_path, 'r')
       f.seek(0, IO::SEEK_END)
       loop do
         content = f.read
