961887aaf5d31e9f5a04caeb482cbbe88ce8c4f4
Cody Cutrer <cody@instructure.com>
2021-08-09 09:03:00 -0600

    add index to speed up the n_strand delete trigger
    some of those queries can get quite slow when the strand gets large
    
    Change-Id: I1861b4b00d6eb8693dcebbb3299a0fc2d568b42e
    Reviewed-on: https://gerrit.instructure.com/c/inst-jobs/+/270962
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Simon Williams <simon@instructure.com>

---
+++ db/migrate/20210809145804_add_n_strand_index.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddNStrandIndex < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    add_index :delayed_jobs, [:strand, :next_in_strand, :id],
+              name: 'n_strand_index',
+              where: 'strand IS NOT NULL',
+              algorithm: :concurrently
+  end
+end
