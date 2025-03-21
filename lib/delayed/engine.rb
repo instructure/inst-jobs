abff6054600fb78ca9b587bdab9cccd5a7475a69
Aaron Ogata <aogata@instructure.com>
2025-03-21 13:31:55 -0700

    fix Rails 7.0

--- lib/delayed/engine.rb
+++ lib/delayed/engine.rb
@@ -4,7 +4,9 @@ module Delayed
   class Engine < ::Rails::Engine
     initializer "delayed_job.set_reloader_hook" do |app|
       Delayed::Worker.lifecycle.around(:perform) do |worker, job, &block|
-        app.reloader.wrap(source: "application.delayed_job") do
+        kwargs = (Rails.version < "7.1") ? {} : { source: "application.delayed_job" }
+
+        app.reloader.wrap(**kwargs) do
           block.call(worker, job)
         end
       end
